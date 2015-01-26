b4c7cb05b38054bf73dd3b7cc34c66604b24c135
sandeep_danta <sandeep.danta@gmail.com>
2015-01-26 18:43:36 +0530

    Removing unused code

--- mapBuilder.js
+++ mapBuilder.js
@@ -8,27 +8,9 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
   var geocoder = new google.maps.Geocoder();
-
-  //Set the current detected location as the center
-  //if(navigator.geolocation) {
-  //  navigator.geolocation.getCurrentPosition(function(position) {
-  //    var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  //    console.log(pos);
-  //    var infoWindow = new google.maps.InfoWindow({
-  //      'map': map,
-  //      'position': pos,
-  //      'content': 'You are here!'
-  //    });
-  //    map.setCenter(pos);
-  //  }, function() {
-  //    handleNoGeoLocation(true);
-  //  });
-  //} else {
-  //  handleNoGeoLocation(false);
-  //}
-
   var pos = new google.maps.LatLng(1.29, 103.85);
   map.setCenter(pos);
+
   google.maps.event.addListener(map, 'click', function(e) {
     geocoder.geocode({'latLng': e.latLng}, function(results, status) {
       if(status == google.maps.GeocoderStatus.OK) {
@@ -50,23 +32,6 @@ function initialize() {
       }
     });
   });
-}
-
-function handleNoGeolocation(errorFlag) {
-  if (errorFlag) {
-    var content = 'Error: The Geolocation service failed.';
-  } else {
-    var content = 'Error: Your browser doesn\'t support geolocation.';
-  }
-
-  var options = {
-    map: map,
-    position: new google.maps.LatLng(60, 105),
-    content: content
-  };
-
-  var infoWindow = new google.maps.InfoWindow(options);
-  map.setCenter(options.position);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
